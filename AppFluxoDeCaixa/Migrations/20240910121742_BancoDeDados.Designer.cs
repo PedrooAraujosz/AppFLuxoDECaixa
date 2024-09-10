34baaf901dd84d5e1360a3a5e1aa5e865f0ec8aa
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-09-10 15:11:10 -0300

    AppFluxoDeDados

--- AppFluxoDeCaixa/Migrations/20240910121742_BancoDeDados.Designer.cs
+++ AppFluxoDeCaixa/Migrations/20240910121742_BancoDeDados.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppFluxoDeCaixa.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240904120513_Inicial")]
-    partial class Inicial
+    [Migration("20240910121742_BancoDeDados")]
+    partial class BancoDeDados
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
